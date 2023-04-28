2a87d9dfc9bb181b82d761f980fbc88d06fee4f4
Robert <rlhead94@gmail.com>
2023-04-28 12:49:53 -0400

    updated hw

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
 
@@ -6,11 +7,27 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+app.use(morgan('dev'))
+app.use(express.static('public'))
+
 const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
   ];
+
+//   app.use((req,res, next) =>{
+//     console.log('new request made');
+//     console.log('host', req.hostname);
+//     console.log('path', req.path);
+//     console.log('method',req.method);
+//     next();
+// } )
+
+// app.use((req, res, next) => {
+//     console.log('in the next middleware');
+//      next();
+//    });
 
 app.get('/', (req, res) => {
     res.render('index', {title:'Home', blogs});
@@ -20,10 +37,11 @@ app.get('/about', (req, res) => {
     res.render('about', {title:'about'})
 })
 
-app.get('/blogs/create', (req, res) => {
+app.get('/create', (req, res) => {
     res.render('create', {title:'Create a new blog'})
 })
 
 app.use((req, res) =>{
     res.status(404).render('404', {title:'404'})
 })
+
